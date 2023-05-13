c8654267a083bd516061bb0a1545162f5544a64a
volbog <volbog93@gmail.com>
2023-05-13 17:31:44 +0200

    Finish all task

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,14 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int first = number % 10;
+        number/=10;
+        int second = number % 10;
+        number/=10;
+        int third = number % 10;
+        number/=10;
+        int fourd = number %10;
+        System.out.println(first + second + third + fourd);
+
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int first = number % 10;
+        number/=10;
+        int second = number % 10;
+        number/=10;
+        int third = number % 10;
+        System.out.println(third+second*10+first*100);
     }
 }
